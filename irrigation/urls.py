5b6bf25ad4003a359f12ac36033ab1b02f03b489
live_morris <nduwayomorris@gmail.com>
2025-07-06 11:33:45 +0300

    checking env v1

--- irrigation/urls.py
+++ irrigation/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('download-data/', views.download_data, name='download-data'),
     path('env-check/', EnvCheckView, name='env_check'),
     path('cron/notifications/', trigger_notifications, name='trigger_notifications'),
-    path('env-check/', views.env_check, name='env-check'),
 ]
