ff28df8a6ea1d589f3c45bc963958cab991cbd0e
live_morris <nduwayomorris@gmail.com>
2025-08-14 22:39:31 +0300

    Adding a bot and more functionality_2

--- smart_irrigation/wsgi.py
+++ smart_irrigation/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import eventlet
+eventlet.monkey_patch()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_irrigation.settings')
 application = get_wsgi_application()
