1fc702a002aac211ac4c3ced295a14d9a6adb41d
live_morris <nduwayomorris@gmail.com>
2025-09-13 10:35:34 +0300

    addes sms configuration1

--- build.sh
+++ build.sh
@@ -8,16 +8,6 @@ pip install -r requirements_remote.txt
 mkdir -p media/profile_pics
 mkdir -p accounts/static/
 python manage.py collectstatic --noinput
-python manage.py migrate_media
-python manage.py migrate_media_to_cloudinary
-python manage.py cleanup_media
-python manage.py cleanup_broken_media
-python manage.py upload_default_avatar
-python manage.py debug_profile_pictures
-python manage.py test_cloudinary
-python manage.py test_cloudinary_upload
-python manage.py debug_cloudinary_storage
-python manage.py test_storage_urls
-python manage.py cleanup_cloudinary
+python manage.py makemigrations
 python manage.py migrate
 
